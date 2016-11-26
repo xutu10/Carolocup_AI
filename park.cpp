06d8d711dedeada4114eb3668bf98395eec31132
xutu <tu.xu@student.hs-rm.de>
2016-11-26 16:23:03 +0100

    version2 formatted

--- park.cpp
+++ park.cpp
@@ -2,97 +2,98 @@
 #include <iostream>
 using namespace std;
 
-Park::Park(){
+Park::Park() {
 	status = 0;
 	substatus = 0;
 	sensor = new Sensor();
 }
 
-Park::~Park(){
+Park::~Park() {
 	delete sensor;
 }
 
-int Park::controlling(){
-	switch(status){
-	case 1: finding();
-		break;
-	case 2: backing();
-		break;
-	case 3: return 1;  // park finish		  
+int Park::controlling() {
+	switch (status) {
+		case 1: finding();
+			break;
+		case 2: backing();
+			break;
+		case 3: return 1; /* park finish */
 	}
 	return 0;
 }
+
 /*return the value of status,either 1 stands for finding(),or 2 for backing() */
 int Park::finding() {
 
 	double current_speed, timer = 0;
-	
-	if (0 == substatus && true  == sensor->side_sensor)   {
+
+	if (0 == substatus && true  == sensor->side_sensor) {
 		substatus = 1;
 		/* ??? stop the timer */
-	}else if(1 == substatus && false == sensor->side_sensor){
+	} else if (1 == substatus && false == sensor->side_sensor)   {
 		substatus = 2;
 		/*start timer */
-	}else if(2 == substatus && true == sensor->side_sensor){
+	} else if (2 == substatus && true == sensor->side_sensor)   {
 		substatus = 1;
 		/*stop timer*/
-	}else if (2 == substatus && false == sensor->side_sensor)    {
+	} else if (2 == substatus && false == sensor->side_sensor)  {
 		sensor->current_rotation = 100;            /* get from sensor */
-		current_speed = TIRE_DIAMETER *sensor-> current_rotation / 60;   /* m/s */
+		current_speed = TIRE_DIAMETER * sensor->current_rotation / 60;   /* m/s */
 		if (current_speed * timer > PARKLOT_LENGTH) {
 			/*stop timer*/
-			return 2; // to call backing()
+			return 2; /* to call backing() */
 		}
 	}
 	return 1;
 }
 
 /*return the value of status either 2 for backing or 3 for finish
- disired speed  0.02m/s */
+ *  disired speed  0.02m/s */
 int Park::backing() {
 
 	if (0 == substatus) {
 		if (0 != sensor->back_dis) {         /*???no change, back right 30 */
-			sensor->set_rotation = -50;       
-			sensor->set_steering = 3.14 / 6;     
-		} else  {                               
+			sensor->set_rotation = -50;
+			sensor->set_steering = 3.14 / 6;
+		} else {
 			substatus = 1;                     /*back left 30*/
 			sensor->set_rotation = -50;
-			sensor->set_steering = -3.14 / 6;   
+			sensor->set_steering = -3.14 / 6;
 		}
-	} else if (1 == substatus)    {
-		if ( sensor->back_dis < 0.05) {
+	} else if (1 == substatus) {
+		if (sensor->back_dis < 0.05) {
 			substatus = 2;                        /*forward right 20*/
 			sensor->set_rotation = 50;
 			sensor->set_steering = 3.14 / 9;
-		} else  {                                /*no change back left 30*/
+		} else {                                 /*no change back left 30*/
 			substatus = 1;
 			sensor->set_rotation = -50;
-			sensor->set_steering = -3.14 / 6;    
+			sensor->set_steering = -3.14 / 6;
 		}
-	} else if (2 == substatus)    {
+	} else if (2 == substatus) {
 		if (sensor->back_dis > 0.2) {             /*back left 20*/
 			substatus = 3;
 			sensor->set_rotation = -50;
 			sensor->set_steering = -3.14 / 9;
-		} else  {                                /*no change forward right 20*/
+		} else {                                 /*no change forward right 20*/
 			sensor->set_rotation = 50;
 			sensor->set_steering = 3.14 / 9;
 		}
-	} else if (3 == substatus)   {
+	} else if (3 == substatus) {
 		if (sensor->back_dis > 0.2) {            /*forward right 10*/
 			substatus = 4;
 			sensor->set_rotation = 50;
 			sensor->set_steering = 3.14 / 18;
-		}else{                                  /*no change back left 20*/
+		} else  {                                /*no change back left 20*/
 			sensor->set_rotation = -50;
 			sensor->set_steering = -3.14 / 9;
 		}
-	} else if (4 == substatus)    {
+	} else if (4 == substatus) {
 		if (sensor->back_dis > 0.12) {
 			/*set rotation and steering 0, blink the light*/
 			return 2;  /*park is finished*/
-		} else  {                             /*no change forward right 10*/
+		} else {                              /*no change forward right 10*/
 			sensor->set_rotation = 50;
 			sensor->set_steering = 3.14 / 18;
 		}
--- park.hpp
+++ park.hpp
@@ -3,31 +3,33 @@
 #define TIRE_DIAMETER  0.05      /* 0.05m = 50mm */
 #define PARKLOT_LENGTH 0.6  /* 0.6m = 600 mm */
 
-/*??? sensor,rotation,steering as parameter to pass or as class member  */
-/*how to set the timer and blink */
-/*??? the value of back_sensor when too far awary from something back */
+/*
+ *??? sensor,rotation,steering as parameter to pass or as class member
+ *how to set the timer and blink
+ *??? the value of back_sensor when too far awary from something back */
 
-class Sensor{
+class Sensor {
 public:
-	double current_rotation;
-	bool side_sensor; /* side_sensor == true means something beside */
+double current_rotation;
+bool side_sensor;     /* side_sensor == true means something beside */
 
-	double back_dis;
-	double set_rotation;
-	double set_steering; /* negative steering is left */
+double back_dis;
+double set_rotation;
+double set_steering;     /* negative steering is left */
 };
 
 class Park {
 public:
-	Park();
-	~Park();
-	int controlling();
-	int finding();
-	int backing();
-	 /* flag to call the finding() or parking()
-		1 stands for finding, 2 for parking, 3 for finish  */
-	int status;  
-	int substatus;   /* status in finding or parking */
-	Sensor* sensor; 
+Park();
+~Park();
+int controlling();
+int finding();
+int backing();
+
+/* flag to call the finding() or parking()
+ *  1 stands for finding, 2 for parking, 3 for finish  */
+int status;
+int substatus;       /* status in finding or parking */
+Sensor *sensor;
 };
 
