07d10fe3511e222ff57cecc5cfea954845920862
xutu <tu.xu@student.hs-rm.de>
2016-11-27 17:51:06 +0100

    formatted version2

--- park.cpp
+++ park.cpp
@@ -14,9 +14,9 @@ Park::~Park() {
 
 int Park::controlling() {
 	switch (status) {
-		case 1: finding();
+		case 1: status = finding();
 			break;
-		case 2: backing();
+		case 2: status = backing();
 			break;
 		case 3: return 1; /* park finish */
 	}
@@ -31,13 +31,13 @@ int Park::finding() {
 	if (0 == substatus && true  == sensor->side_sensor) {
 		substatus = 1;
 		/* ??? stop the timer */
-	} else if (1 == substatus && false == sensor->side_sensor)   {
+	} else if (1 == substatus && false == sensor->side_sensor) {
 		substatus = 2;
 		/*start timer */
-	} else if (2 == substatus && true == sensor->side_sensor)   {
+	} else if (2 == substatus && true == sensor->side_sensor) {
 		substatus = 1;
 		/*stop timer*/
-	} else if (2 == substatus && false == sensor->side_sensor)  {
+	} else if (2 == substatus && false == sensor->side_sensor) {
 		sensor->current_rotation = 100;            /* get from sensor */
 		current_speed = TIRE_DIAMETER * sensor->current_rotation / 60;   /* m/s */
 		if (current_speed * timer > PARKLOT_LENGTH) {
@@ -53,43 +53,54 @@ int Park::finding() {
 int Park::backing() {
 
 	if (0 == substatus) {
-		if (0 != sensor->back_dis) {         /*???no change, back right 30 */
+		/*
+		 *??? the value when noting in the range of back sensor
+		 *??? everytime must set the rotation and steering despite no status changed */
+		if (0 != sensor->back_dis) {         /* back right 30 */
+			substatus = 1;
 			sensor->set_rotation = -50;
 			sensor->set_steering = 3.14 / 6;
-		} else {
-			substatus = 1;                     /*back left 30*/
+		} else {                             /*no change, back right 30 */
+			sensor->set_rotation = -50;
+			sensor->set_steering = 3.14 / 6;
+		}
+	} else if (1 == substatus)  {
+		if (0 == sensor->back_dis) {
+			substatus = 2;                     /*back left 30*/
 			sensor->set_rotation = -50;
 			sensor->set_steering = -3.14 / 6;
+		} else {                              /*no change, back right 30 */
+			sensor->set_rotation = -50;
+			sensor->set_steering = 3.14 / 6;
 		}
-	} else if (1 == substatus) {
+	} else if (2 == substatus)  {
 		if (sensor->back_dis < 0.05) {
-			substatus = 2;                        /*forward right 20*/
+			substatus = 3;                        /*forward right 20*/
 			sensor->set_rotation = 50;
 			sensor->set_steering = 3.14 / 9;
 		} else {                                 /*no change back left 30*/
-			substatus = 1;
 			sensor->set_rotation = -50;
 			sensor->set_steering = -3.14 / 6;
 		}
-	} else if (2 == substatus) {
+	} else if (3 == substatus) {
 		if (sensor->back_dis > 0.2) {             /*back left 20*/
-			substatus = 3;
+			substatus = 4;
 			sensor->set_rotation = -50;
 			sensor->set_steering = -3.14 / 9;
 		} else {                                 /*no change forward right 20*/
 			sensor->set_rotation = 50;
 			sensor->set_steering = 3.14 / 9;
 		}
-	} else if (3 == substatus) {
+	} else if (4 == substatus) {
 		if (sensor->back_dis > 0.2) {            /*forward right 10*/
-			substatus = 4;
+			substatus = 5;
 			sensor->set_rotation = 50;
 			sensor->set_steering = 3.14 / 18;
-		} else  {                                /*no change back left 20*/
+		} else {                                 /*no change back left 20*/
 			sensor->set_rotation = -50;
 			sensor->set_steering = -3.14 / 9;
 		}
-	} else if (4 == substatus) {
+	} else if (5 == substatus) {
 		if (sensor->back_dis > 0.12) {
 			/*set rotation and steering 0, blink the light*/
 			return 2;  /*park is finished*/
