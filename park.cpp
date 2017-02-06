6a05ef766b11d94868dfffee1796298fd3f73e78
xutu <billxu89@gmail.com>
2017-02-06 22:35:32 +0100

    fixed from alec

--- park.cpp
+++ park.cpp
@@ -1,7 +1,5 @@
 #include "park.h"
-#include <iostream>
-using namespace std;
-
+using namespace ki;
 /**
  *  control the whole parking process, according to the status
  *  call the corresponding function
@@ -9,27 +7,28 @@ using namespace std;
  *  @return 1 to main, if the parking finished
  */
 
+Park::Park(carolo_telemetry *tel, SpeedSteering *ss): tele(tel), s_s(ss), status(FINDING),substatus(0){}
+
 int Park::controlling() {
 	switch (status) {
-		case 0: status = finding();
+		case FINDING: status = finding();
 			break;
-		case 1: status = backing();
+		case BACKING: status = backing();
 			break;
-		case 2: return 1; /* park finish */
+		case FINISHED: return 1; /* park finish */
 	}
 	return 0;
 }
 
 /**
- *  look for a park lot whose length is greater than 600mm
- *  side_sensor, distance from telemetry struct are needed
+ *  look for a park space whose length is greater than 0.6m
+ *  side_sensor, distance mebmer in telemetry struct are needed
  *
  *  @return the current status back to controlling(),
- *  either 0 stands for finding(), or 1 for backing()
  */
 int Park::finding() {
 
-	bool side_sensor = false;        /* !!! it should be from telemetry struct */
+	bool side_sensor = false;        /* !!! it should be from telemetry struct. But so far it's broken */
 	if (0 == substatus && true  == side_sensor) {
 		substatus = 1;
 	} else if (1 == substatus && false == side_sensor) {
@@ -41,10 +40,10 @@ int Park::finding() {
 	} else if (2 == substatus && false == side_sensor) {
 		substatus = 3;
 	} else if (3 == substatus) {
-		if (tele->distance - pre_distance > PARKLOT_LENGTH) {
+		if (tele->distance - pre_distance > PARK_SPACE) {
 			pre_distance = 0;                        /*stop countring*/
 			substatus = 0;                           /*initial for backing*/
-			return 1;                                /* to call backing() */
+			return BACKING;                                /* to call backing() */
 		} else  {
 			if (true == side_sensor) {
 				pre_distance = 0;
@@ -53,7 +52,7 @@ int Park::finding() {
 		}
 	}
 s_s->reg(0,0.1);
-return 0;
+return FINDING;
 }
 
 /**
@@ -66,36 +65,38 @@ return 0;
  */
 int Park::backing() {
 	float x = 0;
-	float max_speed = 0.05;
-
+	// ales ? move to config
+	float maxspeed = 0.05;
+	float tmp_backsensor = tele->us[0];
+	
 	if (0 == substatus) {
-		if (0 != tele->us[0]) {         /* back right 30 */
+		if (0 != tmp_backsensor) {         /* back right 30 */
 			substatus = 1;
 			x = 1;
 		} else {                        /*initiallizing back right 30 */
 			x = 1;
 		}
-	} else if (1 == substatus && 0 == tele->us[0]) {     /*back left 30*/
+	} else if (1 == substatus && 0 == tmp_backsensor) {     /*back left 30*/
 		substatus = 2;
 		x = -1;
 	}
-  else if (2 == substatus && tele->us[0] < 0.05) {        /*forward right 20*/
+  else if (2 == substatus && tmp_backsensor < 0.05) {        /*forward right 20*/
 	substatus = 3;
-	x = 2 / 3;
+	x = 2. / 3;
 }
- else if (3 == substatus && tele->us[0] > 0.15) {                 /*back left 20*/
+ else if (3 == substatus && tmp_backsensor > 0.15) {                 /*back left 20*/
 	substatus = 4;
-	x = -2 / 3;
+	x = -2. / 3;
 }
- else if (4 == substatus && tele->us[0] < 0.05) {              /*forward right 10*/
+ else if (4 == substatus && tmp_backsensor < 0.05) {              /*forward right 10*/
 	substatus = 5;
-	x = 1 / 3;
+	x = 1. / 3;
 }
- else if (5 == substatus && tele->us[0] > 0.12) {
+ else if (5 == substatus && tmp_backsensor > 0.12) {
 	s_s->reg(0, 0);                        /*stop the car*/                                  
-	return 2;                              /*park is finished*/
+	return FINISHED;                              /*park is finished*/
 }
 
-s_s->reg(x, max_speed);
-return 1;
+s_s->reg(x, maxspeed);
+return BACKING;
 }
--- park.h
+++ park.h
@@ -1,22 +1,36 @@
-#pragma once
-#include "speed_reg.h"
+#ifndef PARK_H_
+#define PARK_H_
 
-#define TIRE_DIAMETER  0.067    /*unit m */
-#define PARKLOT_LENGTH 0.6  	/*unit m */
+#include "speed_reg.h"
+#include "carolo.h"
+
+
+namespace ki{
 
 class Park {
-	public:
- Park(): status(0), substatus(0) {};
+	
+ public:
+	    Park(carolo_telemetry*,SpeedSteering*);
+		enum status{FINDING, BACKING, FINISHED};
+		
+ private:
 		int controlling();
 		int finding();
 		int backing();
 
 		int status; /*   flag to call the finding() or parking()
 	                 *  1 stands for finding, 2 for parking, 3 for finish*/
-
 		int substatus; /* status in finding or parking */
 		float pre_distance; /*store the previous distance */
 		carolo_telemetry *tele;
 		SpeedSteering *s_s;  /*to call reg in speed_reg.h  */
+		const float PARK_SPACE = 0.6; 
 };
+ 
+}
+
+#endif
+
+
+
 
